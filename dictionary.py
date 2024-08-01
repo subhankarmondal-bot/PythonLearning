2118636c12178eee04f2999182bd25620a6ab243
Bhaskar <bhaskarmondal860@gmail.com>
2024-08-01 21:36:39 +0530

    dict example

---
+++ dictionary.py
@@ -0,0 +1,9 @@
+def main() : 
+    myDict = {
+        "bhaskar" : 25, # name : age
+        "subhankar" : 15,
+        "subimal" : 55,
+        "arundhuti" : 44
+    }
+    print(myDict["bhaskar"])
+main()
