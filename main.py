ac74a1e1ba95b7884eb21e06046c45e9fa43b7e1
Bhaskar <bhaskarmondal860@gmail.com>
2024-08-01 21:28:47 +0530

    tuple and list example

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 def main () : 
-    print("hello world")
+    mytuple = (1, 2, 3, 4, 5)  # value 1
+    
+    # tuples are indexed
+    print(mytuple[1])
+    print(mytuple[4])
+    
+    # tuples contain duplicate elements
+    mytuple = (1, 2, 3, 4, 2, 3) # value 2
+    print(mytuple)
+    
+    # adding an element
+    # mytuple[1] = 100 
+    # print(mytuple)
 
+    myList = [1,2,3,4,5]
+    myList[0] = 100
+    print(myList)
 main()
